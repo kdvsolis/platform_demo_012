6e117df4fdf0b389b5bbbeb6bca0347f3f522b1f
kdvsolis <kdvsolis@gmail.com>
2023-09-03 01:37:47 +0800

    Initial Commit

---
+++ PlatformDemo/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformDemo.Models;
+using System.Reflection;
+
+namespace PlatformDemo
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.SeedData(); // Call the data seeding method
+        }
+    }
+}
